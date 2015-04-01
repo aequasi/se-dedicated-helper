a0d0fe154e9a4d1cafac2306da172e56bff5b2fa
Aaron Scherer <aequasi@gmail.com>
2015-03-31 22:32:12 -0700

    Updating index

--- web/index.php
+++ web/index.php
@@ -9,10 +9,12 @@
  * with this source code in the file LICENSE
  */
 
+/*
 $filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return false;
 }
+*/
 
 $app = require __DIR__.'/../src/app.php';
 $app->run();
